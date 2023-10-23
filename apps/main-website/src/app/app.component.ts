bc1ed412bb7a0bb6ac5a8765131fdcbaa23f7a0c
Thomas Ghys <thomas-ghys@outlook.com>
2023-10-23 21:45:12 +0200

    Creation of standalone application

---
+++ apps/main-website/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+	standalone: true,
+	imports: [
+		RouterModule
+	],
+	selector: 'personal-website-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+	title = 'main-website';
+}
