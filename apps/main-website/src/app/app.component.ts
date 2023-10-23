59a5f91e43889e40bf2f77ebaf70af9ca6741582
Thomas Ghys <thomas-ghys@outlook.com>
2023-10-23 21:14:21 +0200

    Get linter up and running

--- apps/main-website/src/app/app.component.ts
+++ apps/main-website/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   imports: [NxWelcomeComponent, RouterModule],
   selector: 'personal-website-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'main-website';
