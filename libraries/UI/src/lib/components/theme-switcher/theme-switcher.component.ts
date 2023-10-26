b783df817125012a60eda3ed5d7727a3bb9585b7
Thomas Ghys <thomas-ghys@outlook.com>
2023-10-26 22:09:52 +0200

    Add theme switcher to the main site

---
+++ libraries/UI/src/lib/components/theme-switcher/theme-switcher.component.ts
@@ -0,0 +1,35 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+	selector: 'lib-ui-theme-switcher',
+	standalone: true,
+	imports: [
+		CommonModule, 
+		MatButtonModule
+	],
+	templateUrl: './theme-switcher.component.html',
+	styleUrls: ['./theme-switcher.component.scss'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ThemeSwitcherComponent implements OnInit {
+	protected userTheme: 'light' | 'dark' = 'light';
+	private rootApplication: HTMLElement = document.getElementsByTagName("html")[0];
+
+	ngOnInit(): void {
+		if (this.rootApplication.hasAttribute("data-theme")) {
+			if (this.rootApplication.getAttribute("data-theme") === 'dark-theme') {
+				this.userTheme = 'dark';
+				return;
+			}
+			this.userTheme = 'light';
+			return;
+		}
+	}
+
+	setDataTheme(newTheme: 'light' | 'dark') {
+		this.rootApplication.setAttribute("data-theme", newTheme);
+		this.userTheme = newTheme;
+	}
+}
--- libraries/UI/src/lib/layout/menu-bar/menu-bar.component.ts
+++ libraries/UI/src/lib/layout/menu-bar/menu-bar.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ThemeSwitcherComponent } from '../../components/theme-switcher/theme-switcher.component';
 
 @Component({
 	selector: 'lib-ui-menu-bar',
 	standalone: true,
 	imports: [
-		CommonModule
+		CommonModule,
+		ThemeSwitcherComponent
 	],
 	templateUrl: './menu-bar.component.html',
 	styleUrls: ['./menu-bar.component.scss'],
