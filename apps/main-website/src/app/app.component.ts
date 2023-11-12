82b34d66cdbf6fbe4bb8ed0073c00446a81fb6a7
Thomas Ghys <thomas-ghys@outlook.com>
2023-11-12 16:19:56 +0100

    Create working translation system

--- apps/main-website/src/app/app.component.ts
+++ apps/main-website/src/app/app.component.ts
@@ -1,12 +1,16 @@
+import { TranslateService } from '@ngx-translate/core';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MenuBarComponent } from '@personal-website/UI';
+import { Title } from '@angular/platform-browser';
+import { SharingModule } from './sharing/sharing.module';
 
 @Component({
 	standalone: true,
 	imports: [
 		RouterModule,
 		MenuBarComponent,
+		SharingModule
 	],
 	selector: 'personal-website-root',
 	templateUrl: './app.component.html',
@@ -14,4 +18,11 @@ import { MenuBarComponent } from '@personal-website/UI';
 })
 export class AppComponent {
 	title = 'main-website';
+
+	constructor(private titleService: Title, private translateService: TranslateService) {
+		this.titleService.setTitle(`${this.title}`);
+
+		translateService.setDefaultLang('en');
+		translateService.use('en');
+	}
 }
